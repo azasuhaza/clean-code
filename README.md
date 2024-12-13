048458767429786515e1fdd14fa45379ae228b70
azasuhaza <azasuhaza@gmail.com>
2024-12-13 21:49:56 +0800

    initial check-in

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # clean-code
+
+1. mvn project that contain clean codes
+2. springboot project
---
+++ cleancode-mvn/src/main/java/com/aza/suhaza/cleancode/App.java
@@ -0,0 +1,10 @@
+package com.aza.suhaza.cleancode;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
